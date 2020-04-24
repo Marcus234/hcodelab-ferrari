ba0a42c9ebae71180461935f0958696ba21b0d0c
Marcus Ribeiro <marcusrex234@hotmail.com>
2020-04-24 20:12:34 -0300

    Criei o repositório

---
+++ .next/static/webpack/static/development/pages/index.js.36e17dd3023a73eabd03.hot-update.js
@@ -0,0 +1,32 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/Menu.js":
+/*!****************************!*\
+  !*** ./components/Menu.js ***!
+  \****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Index; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Projetos\\hcodelab\\components\\Menu.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+function Index() {
+  return __jsx("h1", {
+    __self: this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 9
+    }
+  }, "Estou no Menu");
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.36e17dd3023a73eabd03.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+
+export default function Index() {
+    return(
+        <div>
+            <Header />
+                <h1>Estou no Index</h1>
+        </div>
+    )
+}
